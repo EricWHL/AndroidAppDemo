2c5f689cc370d01a74c92eef5c51731e95379ee5
WangHailong <396568798@qq.com>
2021-02-02 18:06:37 +0800

    fix recycle view

--- app/src/main/java/com/example/androidappdemo/Activity/RecycleViewActivity.java
+++ app/src/main/java/com/example/androidappdemo/Activity/RecycleViewActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.androidappdemo.Adapter.ListItem;
 import com.example.androidappdemo.Adapter.RAdapter;
@@ -18,6 +19,14 @@ import java.util.List;
 
 public class RecycleViewActivity extends Activity {
     final String TAG = "RecycleViewActivity";
+
+    private final int LAYOUT_VERTICAL_LIST    = 0;
+    private final int LAYOUT_HORIZONTAL_LIST  = 1;
+    private final int LAYOUT_VERTICAL_GRID    = 2;
+    private final int LAYOUT_HORIZONTAL_GRID  = 3;
+
+    LinearLayoutManager mLinerLayoutMgr;
+    StaggeredGridLayoutManager mGridLayoutMgr;
 
     private RecyclerView mView;
     private List<ListItem> mList = new ArrayList<ListItem>();
@@ -30,9 +39,10 @@ public class RecycleViewActivity extends Activity {
         initView();
         addData();
 
-        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mLinerLayoutMgr = new LinearLayoutManager(this);
+        mGridLayoutMgr = new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL);
+        switchLayout(LAYOUT_VERTICAL_LIST);
 
-        mView.setLayoutManager(manager);
         mAdapter = new RAdapter(mList);
         mView.setAdapter(mAdapter);
     }
@@ -41,14 +51,49 @@ public class RecycleViewActivity extends Activity {
         Log.d(TAG,"onClick");
         switch(v.getId()) {
             case R.id.vertical_list:
+                switchLayout(LAYOUT_VERTICAL_LIST);
                 Log.d(TAG,"vertical_list button clicked");
                 break;
-            case R.id.Detail_list:
+            case R.id.horizontal_list:
+                switchLayout(LAYOUT_HORIZONTAL_LIST);
                 Log.d(TAG,"Detail_list button clicked");
 
                 break;
-            case R.id.List:
-                Log.d(TAG,"List button clicked");
+            case R.id.vertical_grid:
+                switchLayout(LAYOUT_VERTICAL_GRID);
+                Log.d(TAG,"vertical_grid button clicked");
+                break;
+            case R.id.horizontal_grid:
+                switchLayout(LAYOUT_HORIZONTAL_GRID);
+                Log.d(TAG,"horizontal_grid button clicked");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void switchLayout(int type) {
+        Log.d(TAG,"switchLayout");
+        switch (type) {
+            case LAYOUT_VERTICAL_LIST:
+                Log.d(TAG,"switchLayout to LAYOUT_VERTICAL");
+                mLinerLayoutMgr.setOrientation(LinearLayoutManager.VERTICAL);
+                mView.setLayoutManager(mLinerLayoutMgr);
+                break;
+            case LAYOUT_HORIZONTAL_LIST:
+                Log.d(TAG,"switchLayout to LAYOUT_DETAIL");
+                mLinerLayoutMgr.setOrientation(LinearLayoutManager.HORIZONTAL);
+                mView.setLayoutManager(mLinerLayoutMgr);
+                break;
+            case LAYOUT_VERTICAL_GRID:
+                Log.d(TAG,"switchLayout to LAYOUT_VERTICAL_GRID");
+                mGridLayoutMgr.setOrientation(StaggeredGridLayoutManager.VERTICAL);
+                mView.setLayoutManager(mGridLayoutMgr);
+                break;
+            case LAYOUT_HORIZONTAL_GRID:
+                Log.d(TAG,"switchLayout to LAYOUT_HORIZONTAL_GRID");
+                mGridLayoutMgr.setOrientation(StaggeredGridLayoutManager.HORIZONTAL);
+                mView.setLayoutManager(mGridLayoutMgr);
                 break;
             default:
                 break;
--- app/src/main/java/com/example/androidappdemo/Adapter/RAdapter.java
+++ app/src/main/java/com/example/androidappdemo/Adapter/RAdapter.java
@@ -4,6 +4,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,6 +27,29 @@ public class RAdapter extends RecyclerView.Adapter<RAdapter.ViewHolder> {
         View view = View.inflate(parent.getContext(),R.layout.list_item,null);
         //实例化ViewHolder
         ViewHolder holder = new ViewHolder(view);
+
+        //item设置监听
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取到position
+                int layoutPosition = holder.getLayoutPosition();
+                //土司
+                Toast.makeText(view.getContext(),"This is item :"+layoutPosition,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //textView设置监听
+        holder.text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取到position
+                int layoutPosition = holder.getLayoutPosition();
+                //土司
+                Toast.makeText(view.getContext(),"This is textView :"+layoutPosition,Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return holder;
     }
 
@@ -50,8 +74,11 @@ public class RAdapter extends RecyclerView.Adapter<RAdapter.ViewHolder> {
         //行布局中的控件
         ImageView img;
         TextView text;
+        //视图
+        View view;
         public ViewHolder(View itemView) {
             super(itemView);
+            view = itemView;
             //绑定控件
             img = (ImageView) itemView.findViewById(R.id.item_img);
             text = (TextView) itemView.findViewById(R.id.item_text);
