81121c3b4648c2e276910a353322ecb4701d5594
WangHailong <396568798@qq.com>
2021-02-02 16:10:30 +0800

    add activity code

--- app/src/main/java/com/example/androidappdemo/Activity/EditorActivity.java
+++ app/src/main/java/com/example/androidappdemo/Activity/EditorActivity.java
@@ -1,6 +1,20 @@
 package com.example.androidappdemo.Activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.example.androidappdemo.R;
 
 public class EditorActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+    public void onClick(View view) {
+    }
 }
--- app/src/main/java/com/example/androidappdemo/Activity/MainActivity.java
+++ app/src/main/java/com/example/androidappdemo/Activity/MainActivity.java
@@ -3,14 +3,49 @@ package com.example.androidappdemo.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.androidappdemo.R;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void onClick(View v) {
+        Log.d(TAG,"onClick");
+        switch(v.getId()) {
+            case R.id.RecycleView:
+                Log.d(TAG,"RecycleView button clicked");
+                break;
+            case R.id.LinearLayout:
+                Log.d(TAG,"LinearLayout button clicked");
+                break;
+            case R.id.RelativeLayout:
+                Log.d(TAG,"RelativeLayout button clicked");
+                break;
+            case R.id.AbsoluteLayout:
+                Log.d(TAG,"AbsoluteLayout button clicked");
+                break;
+            case R.id.FrameLayout:
+                Log.d(TAG,"FrameLayout button clicked");
+                break;
+            case R.id.TableLayout:
+                Log.d(TAG,"TableLayout button clicked");
+                break;
+
+            default:
+                break;
+        }
     }
 }
